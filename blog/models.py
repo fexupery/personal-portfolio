ae04ba347b960c94885691264bb5919548d73765
fexupery <andresfelipeml@gmail.com>
2020-04-12 09:50:49 -0500

    change of Blog model: content data type chaged from char to textfield

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ from django.db.models import fields
 class Blog(models.Model):
     title = fields.CharField(max_length=100)
     date = fields.DateField(auto_now_add=True)
-    content = fields.CharField(max_length=5000, default='no content for this entry')
+    content = fields.TextField(default='no content for this entry')
