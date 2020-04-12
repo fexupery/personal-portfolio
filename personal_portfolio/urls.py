e64715fd6c757d4b09ff5f9979db3ecb2e1b56fe
fexupery <andresfelipeml@gmail.com>
2020-04-12 09:03:44 -0500

    how to assign the responsibility of urls to each app

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -8,7 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.a s_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -18,10 +18,12 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
+from django.urls import include
 
 urlpatterns = [
     path('',views.home,name='home'),
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
