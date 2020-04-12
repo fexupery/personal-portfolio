0b51318c76a69993d1a75b679500111f109523f7
fexupery <andresfelipeml@gmail.com>
2020-04-12 09:54:07 -0500

    parameter of DateField was deleted to allow the user to choose her own date

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ from django.db.models import fields
 
 class Blog(models.Model):
     title = fields.CharField(max_length=100)
-    date = fields.DateField(auto_now_add=True)
+    date = fields.DateField()
     content = fields.TextField(default='no content for this entry')
