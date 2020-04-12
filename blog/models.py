9f5749f3d4c9e719c5b094f3a77a7a7f318a4431
fexupery <andresfelipeml@gmail.com>
2020-04-12 09:44:32 -0500

    showing blogs from database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.db.models import fields
 
-# Create your models here.
+class Blog(models.Model):
+    title = fields.CharField(max_length=100)
+    date = fields.DateField(auto_now_add=True)
+    content = fields.CharField(max_length=5000, default='no content for this entry')
