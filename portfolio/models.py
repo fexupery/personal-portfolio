5e6ef357fa94a28439c93804625d1d34b7c80eab
fexupery <andresfelipeml@gmail.com>
2020-04-18 06:03:11 -0500

    chaning how database admin page list project objects

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,7 @@ class Project(models.Model):
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
+
+    # to show in the admin console the object from database with the title instead of object 1, object 2, ...
+    def __str__(self):
+        return self.title
