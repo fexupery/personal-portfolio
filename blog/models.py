b5aad55c9e373bccac3ed56b253714bd9839e8e1
fexupery <andresfelipeml@gmail.com>
2020-04-18 06:01:06 -0500

    how to change the database admin page show the name of objects

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,7 @@ class Blog(models.Model):
     title = fields.CharField(max_length=100)
     date = fields.DateField()
     content = fields.TextField(default='no content for this entry')
+
+# to show in the admin console the object from database with the title instead of object 1, object 2, ...
+    def __str__(self):
+        return self.title
