0da532681705ca2c50c7e6694b7789e631114e08
fexupery <andresfelipeml@gmail.com>
2020-04-18 05:22:59 -0500

    how to see a blog? getting a blog from the database using its primary key and using it to form an url

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog' #to reference all urls to blog and avoid mistakes with urls to others apps as portfolio
+
 urlpatterns = [
     path('',views.all_blogs,name='all_blogs'),
+    path('<int:blog_id>/',views.detail,name='detail'),
 ]
